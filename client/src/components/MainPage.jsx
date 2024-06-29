e4733fa8f9079d29dde8e8927c343fbe8948a414
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-06-29 16:14:57 -0500

    finished project and deployed to heroku

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -41,7 +41,6 @@ function MainPage() {
 
     try {
       const response = await fetch(
-        // "https://cvl-generator-a8eca88952f3.herokuapp.com/completions",  options );
         "https://cover-letter-generator-ai-b7323a56ca78.herokuapp.com/completions",  options );
         // "http://localhost:3001/completions",  options );
 
